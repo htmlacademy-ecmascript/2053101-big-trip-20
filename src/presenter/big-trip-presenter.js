625336ad5fb7499cea50bd3f87fa2f510bcb9035
ViktorLukashevich <101119183+lvktm@users.noreply.github.com>
2023-04-26 23:40:31 +0300

    1.18.5_Исправит отрисовку точек маршрута в списке (в нужно теге) в главном презентере

--- src/presenter/big-trip-presenter.js
+++ src/presenter/big-trip-presenter.js
@@ -5,16 +5,20 @@ import PointListView from '../view/point-list-view';
 import PointView from '../view/point-view';
 
 export default class BigTripPresenter {
+  #tripEventsContainer = null;
+  #pointListComponent = new PointListView();
+  #pointListContainer = this.#pointListComponent.getElement();
+
   constructor(tripEventsContainer) {
-    this.tripEventsContainer = tripEventsContainer;
+    this.#tripEventsContainer = tripEventsContainer;
   }
 
   init = () => {
-    render(new PointListView(), this.tripEventsContainer);
-    render(new PointEditView(), this.tripEventsContainer);
+    render(this.#pointListComponent, this.#tripEventsContainer);
+    render(new PointEditView(), this.#pointListContainer);
 
     for(let i = 1; i <= TRIP_EVENT_VIEW_AMOUNT; i++) {
-      render(new PointView(), this.tripEventsContainer);
+      render(new PointView(), this.#pointListContainer);
     }
   };
 
