9bee66857a5059f19cccf798fabf81d3abee9a73
ViktorLukashevich <101119183+lvktm@users.noreply.github.com>
2023-05-01 14:35:18 +0300

    2.13.9_Отрисует список карточек маршрутов
    Создаст два компонента CardView и ListView, импортирует их в type.js. В ListView импортирует компонент CardView и использует его для отрисовки элементов списка. В index.html добавит тег <list-view>. В main.js вызовет метод render DOM-элемента list-view.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './views/add-view.js';
 import './views/brief-view.js';
 import './views/filter-view.js';
 import './views/sort-view.js';
+import './views/list-view.js';
+
 /**
  * @type {BriefView}
  */
@@ -18,8 +20,13 @@ const filterView = document.querySelector('filter-view');
  * @type {SortView}
  */
 const sortView = document.querySelector('sort-view');
+/**
+ * @type {ListView}
+ */
+const listView = document.querySelector('list-view');
 
 brief.render();
 addView.render();
 filterView.render();
 sortView.render();
+listView.render();
--- src/types.js
+++ src/types.js
@@ -7,4 +7,6 @@
  * @typedef {import('./views/add-view.js').default} AddView
  * @typedef {import('./views/filter-view').default} FilterView
  * @typedef {import('./views/sort-view').default} SortView
+ * @typedef {import('./views/list-view').default} ListView
+ * @typedef {import('./views/card-view').default} CardView
  */
